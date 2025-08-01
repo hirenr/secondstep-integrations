25613d306f8bc016126b5a0ad2ad7e41f17c2632
hiren <20088337+hirenr@users.noreply.github.com>
2025-08-01 15:43:33 +0530

    Added integrationUri

--- scripts/build-registry.ts
+++ scripts/build-registry.ts
@@ -4,19 +4,20 @@ import path from 'path'
 import yaml from 'js-yaml'
 import glob from 'fast-glob'
 
-async function loadIntegration(dir: string) {
-  const integrationYamlPath = path.join(dir, 'integration.yaml')
+async function loadIntegration(integrationPath: string) {
+  const integrationYamlPath = path.join(integrationPath, 'integration.yaml')
   const raw = await fs.readFile(integrationYamlPath, 'utf8')
   const integration = yaml.load(raw) as any
 
-  const eventsDir = path.join(dir, 'events')
+  const eventsDir = path.join(integrationPath, 'events')
 
-  const functionPaths = await glob('functions/*/*/function.yaml', { cwd: dir })
+  const functionPaths = await glob('functions/*/*/function.yaml', { cwd: integrationPath })
   const eventFiles = (await fs.readdir(eventsDir).catch(() => []))
 
   return {
     id: integration.id,
     name: integration.name,
+    uri: integrationPath,
     version: integration.version,
     icon: integration.icon,
     category: integration.category,
